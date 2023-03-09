0cc370f981fb0496f4c682c68501a051a4fdb001
eddrindesiderio <61492950+eddrindesiderio@users.noreply.github.com>
2023-03-10 00:15:44 +0800

    Added new files

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Hiiiiiiii",
+    introTitle: 'Si pa Confess nanaman ako HAHAHA',
+    introDesc: `Pag pasensyahan mo nalang me haaa HAAHAHA`,
+    btnIntro: '^^HiHi^^',
+    title: 'DITO NALANG BOSSING HIYA q MAG SABI SAYO',
+    desc: 'KAPEE TAYO BOSS? ',
+    btnYes: 'Yahhhhh <33',
+    btnNo: 'Nahhhhh :3',
+    question: 'Halaaa legit? when Mossing? <3',
+    btnReply: 'OKIIOKIII <3',
+    reply: 'Sige anytime <33333333',
+    mess: 'Okiiii noted pooo HAHAHAH ang keme ko talaga mossing kahit kelan ',
+    messDesc: 'G u?',
+    btnAccept: 'Okiiiii <3',
+    messLink: 'https://www.facebook.com/CofteaCafeMabiniAve' 
+}
